e26f90981f6f8ee171cb8ddaf9aa0a4525f748ca
pavani <pavaniavni456@gmail.com>
2022-02-09 10:44:32 +0530

    added GradeBookDictionar

---
+++ GradeBookDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Collections.Generics
+{
+    class GradeBookDictionary
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grade Book Dictionary : ");
+            Dictionary<string, string> IN = new Dictionary<string, string>();
+            Console.WriteLine("Enter Student Id and Student Name : ");
+            for (int i = 0; i < 3; i++)
+            {
+                IN.Add(Console.ReadLine(), Console.ReadLine());
+            }
+            foreach (var item in IN)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+        }
+    }
+}
